131e2ef3572c9b17aa741d532c66db63b4897b20
Andrew Fray <andrew.fray@gmail.com>
2022-08-31 15:35:23 +0100

    whoops forgot these

--- serialusb.py
+++ serialusb.py
@@ -1,43 +1,40 @@
-import uselect, time, sys
-from picographics import PicoGraphics, DISPLAY_TUFTY_2040
+import uselect
+import time
+import sys
 
 POLL_READ_ONLY = (
     uselect.POLLIN |
     uselect.POLLHUP |
     uselect.POLLERR
 )
-display = PicoGraphics(display=DISPLAY_TUFTY_2040)
-display.set_font("bitmap8")
-BG = display.create_pen(0, 43, 54)
-FG_BODY = display.create_pen(131, 148, 150)
 
-def read_loop():
-    """_summary_ based on https://goldensyrupgames.com/blog/2022-02-04-pico-simple-two-way-serial/
-    """
-    #serial_poll = uselect.poll()
-    #serial_poll.register(sys.stdin, POLL_READ_ONLY)
-    latest = "-"
+class SerialUSB:
+    def __init__(self) -> None:
+        self._listener = uselect.poll()
+        self._listener.register(sys.stdin, POLL_READ_ONLY)
+        self._buffer = ""
+        self._complete = []
+        
+    def update(self):
+        while self._listener.poll(0):
+            new_c = str(sys.stdin.read(1))
+            if new_c == "\n":
+                clean_buffer = self._buffer.strip()
+                if len(clean_buffer) > 0:
+                    self._complete.append(self._buffer)
+                self._buffer = ""
+            else:
+                self._buffer += new_c
+
+    def is_any(self) -> bool: 
+        """
+        Returns:
+            true if you can pop_next()
+        """
+        return len(self._complete) > 0
     
-    while True:
-        #if serial_poll.poll(0):
-            #print("poll heard!")
-            #latest = str(sys.stdin.read(1))
-            
-        select_result = uselect.select([sys.stdin], [], [], 0)
-        while select_result[0]:
-            # there's no easy micropython way to get all the bytes.
-            # instead get the minimum there could be and keep checking with select and a while loop
-            latest = str(sys.stdin.read(1))
-            print("got new " + latest)
-            # check if there's any input remaining to buffer
-            select_result = uselect.select([sys.stdin], [], [], 0)
-            
-        display.set_pen(BG)
-        display.clear()
-        display.set_pen(FG_BODY)
-        display.text("got " + latest, 0, 0)
-        display.update()
-        
-        time.sleep_ms(20)
-        
-read_loop()
+    def pop_next(self):
+        """returns next complete input, if is_any() is true
+        """
+        next = self._complete.pop(0)
+        return next 
